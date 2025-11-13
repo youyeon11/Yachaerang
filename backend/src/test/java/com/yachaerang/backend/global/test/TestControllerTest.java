3800a43fd2f55c85eeaa5ba78c13792f81790272
youyeon11 <yurry45@naver.com>
2025-11-13 21:49:49 +0900

    feat: REST Docs 작성을 위한 WebMVCTest 코드 추가

--- backend/src/test/java/com/yachaerang/backend/global/test/TestControllerTest.java
+++ backend/src/test/java/com/yachaerang/backend/global/test/TestControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.mock;
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import org.springframework.restdocs.payload.FieldDescriptor;
@@ -34,17 +35,29 @@ class TestControllerTest extends RestDocsSupport {
 
 
     // 공통 응답 필드
-    static FieldDescriptor[] envelop = new FieldDescriptor[]{
+    private static final FieldDescriptor[] ENVELOPE_COMMON = new FieldDescriptor[]{
             fieldWithPath("httpStatus").type(STRING).description("HTTP 상태 코드"),
             fieldWithPath("success").type(BOOLEAN).description("응답 성공 여부"),
             fieldWithPath("code").type(STRING).description("응답 코드"),
-            fieldWithPath("message").type(STRING).description("응답 메시지"),
-            fieldWithPath("data").type(OBJECT).description("데이터")
+            fieldWithPath("message").type(STRING).description("응답 메시지")
     };
+
+    /**
+     * data 가 존재하는 경우의 필드
+     */
+    private static final FieldDescriptor DATA_OBJECT_DESCRIPTOR =
+            fieldWithPath("data").type(OBJECT).description("응답 데이터");
+
+    /**
+     * data 가 존재하지 않는 경우(null) 의 필드
+     */
+    private static final FieldDescriptor DATA_NULL_DESCRIPTOR =
+            fieldWithPath("data").type(NULL).description("응답 데이터 (없음)");
+
 
     @Test
     @DisplayName("데이터가 있을 때 성공의 응답인 경우")
-    void success() throws Exception {
+    void 데이터가_있을때의_성공() throws Exception {
         // given
         TestDto expectedDto = new TestDto("test");
         given(testService.successWithData()).willReturn(expectedDto);
@@ -54,11 +67,28 @@ class TestControllerTest extends RestDocsSupport {
                 .andExpect(status().isOk())
                 .andDo(doc(
                         "test-success",
-                        responseFields(envelop)
+                        responseFields(ENVELOPE_COMMON).and(DATA_OBJECT_DESCRIPTOR)
                                 .andWithPrefix("data.",
                                         fieldWithPath("name").type(STRING).description("test")
                                 )
                 ));
 
+    }
+
+    @Test
+    @DisplayName("데이터가 없을 때 성공의 응답인 경우")
+    void 데이터가_없을때의_성공() throws Exception {
+        // given
+        willDoNothing().given(testService).successWithoutData();
+
+        // when & then
+        mockMvc.perform(get("/test/success/without")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(doc(
+                        "test-success-without-data",
+                        responseFields(ENVELOPE_COMMON)
+                                .and(DATA_NULL_DESCRIPTOR)
+                ));
     }
 }
