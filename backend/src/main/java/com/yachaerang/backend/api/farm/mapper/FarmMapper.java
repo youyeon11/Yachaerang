fb28ac8c1d5ab2f3dd08a06531b6b3fc084cde4b
youyeon11 <yurry45@naver.com>
2025-11-18 00:35:32 +0900

    refactor: mapper -> repository 패키지로 리팩토링

--- backend/src/main/java/com/yachaerang/backend/api/farm/mapper/FarmMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yachaerang.backend.api.farm.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface FarmMapper {
-}
--- backend/src/main/java/com/yachaerang/backend/api/product/mapper/ProductMapper.java
+++
@@ -1,7 +0,0 @@
-package com.yachaerang.backend.api.product.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ProductMapper {
-}
