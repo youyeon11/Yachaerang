5d52bd44ad4138ce6631174dfbdf2b5596f187fe
youyeon11 <yurry45@naver.com>
2025-11-18 17:47:41 +0900

    feat: SpringSecurity Configuration 구현

---
+++ backend/src/main/java/com/yachaerang/backend/global/auth/config/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.yachaerang.backend.global.auth.config;
+
+import com.yachaerang.backend.api.member.repository.MemberMapper;
+import com.yachaerang.backend.global.auth.jwt.JwtAuthenticationFilter;
+import com.yachaerang.backend.global.auth.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+/*
+Spring Security 관련 설정 Configuration
+ */
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final MemberMapper memberMapper;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    /*
+    암호화 등록
+     */
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /*
+    Security에 대한 필터 등록
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .cors(httpSecurityCorsConfigurer -> {
+                    httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource());
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagerConfigurer -> {
+                    sessionManagerConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
+
+        setJwtTokenProvider(httpSecurity);
+        return  httpSecurity.build();
+    }
+
+    /*
+    설정한 JWT Filter를 기반으로 Security Filter에 적용
+     */
+    private void setJwtTokenProvider(HttpSecurity httpSecurity) throws Exception {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(memberMapper, jwtTokenProvider);
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    /*
+    CORS 설정
+     */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+}
