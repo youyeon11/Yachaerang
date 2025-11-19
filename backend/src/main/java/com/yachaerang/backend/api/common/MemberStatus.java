f25eb5530b9d20d240f39816425f8e0b2dc2c8b0
youyeon11 <yurry45@naver.com>
2025-11-19 17:50:40 +0900

    fix: MyBatis에서 Enum 타입을 매핑

--- backend/src/main/java/com/yachaerang/backend/api/common/MemberStatus.java
+++ backend/src/main/java/com/yachaerang/backend/api/common/MemberStatus.java
@@ -1,6 +1,27 @@
 package com.yachaerang.backend.api.common;
 
+import com.yachaerang.backend.global.exception.GeneralException;
+import com.yachaerang.backend.global.response.ErrorCode;
+
 public enum MemberStatus {
-    ACTIVE,
-    INACTIVE
+    ACTIVE("active"),
+    INACTIVE("inactive")
+    ;
+
+    private String description;
+    MemberStatus(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public MemberStatus findMemberStatusByDescription(String description) {
+        for (MemberStatus memberStatus : MemberStatus.values()) {
+            if (memberStatus.getDescription().equals(description)) {
+                return memberStatus;
+            }
+        }
+        throw GeneralException.of(ErrorCode.INTERNAL_SERVER_ERROR);
+    }
 }
--- backend/src/main/java/com/yachaerang/backend/api/common/Role.java
+++ backend/src/main/java/com/yachaerang/backend/api/common/Role.java
@@ -1,6 +1,30 @@
 package com.yachaerang.backend.api.common;
 
+import com.yachaerang.backend.global.exception.GeneralException;
+import com.yachaerang.backend.global.response.ErrorCode;
+
 public enum Role {
-    ROLE_USER,
-    ROLE_ADMIN,
+    ROLE_ANONYMOUS("anonymous"),
+    ROLE_USER("user"),
+    ROLE_ADMIN("admin"),
+    ;
+
+    private String description;
+
+    Role(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Role findRoleByDescription(String description) {
+        for (Role role : Role.values()) {
+            if (role.getDescription().equals(description)) {
+                return role;
+            }
+        }
+        throw GeneralException.of(ErrorCode.INTERNAL_SERVER_ERROR);
+    }
 }
