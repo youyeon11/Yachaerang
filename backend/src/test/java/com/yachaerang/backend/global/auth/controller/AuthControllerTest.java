a5356b6b83c0cf9499e4a153144df6193867a9ae
youyeon11 <yurry45@naver.com>
2025-11-19 17:50:01 +0900

    test: 회원 관련 Test 코드 작성

---
+++ backend/src/test/java/com/yachaerang/backend/global/auth/controller/AuthControllerTest.java
@@ -0,0 +1,7 @@
+package com.yachaerang.backend.global.auth.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthControllerTest {
+
+}
---
+++ backend/src/test/java/com/yachaerang/backend/global/auth/service/AuthServiceTest.java
@@ -0,0 +1,7 @@
+package com.yachaerang.backend.global.auth.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest {
+
+}
