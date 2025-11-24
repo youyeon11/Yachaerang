9ee3981a03a2a10d10e091ef623d124674666ea0
youyeon11 <yurry45@naver.com>
2025-11-24 13:47:31 +0900

    feat: 나의 프로필 조회 및 수정 구현

--- backend/src/main/java/com/yachaerang/backend/api/member/dto/response/MemberResponseDto.java
+++ backend/src/main/java/com/yachaerang/backend/api/member/dto/response/MemberResponseDto.java
@@ -1,10 +1,28 @@
 package com.yachaerang.backend.api.member.dto.response;
 
+import lombok.*;
+
 public class MemberResponseDto {
 
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class FindDto {
         private long id;
         private String memberCode;
+        private String email;
+        private String name;
+        private String nickname;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyPageDto {
         private String email;
         private String name;
         private String nickname;
