7cd0d1f85995c8fb17e3dbdf27f73482dc62b093
김가은 <gaeunji1@naver.com>
2025-12-03 17:12:04 +0900

    feat: pinia 설정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.mount('#app');
