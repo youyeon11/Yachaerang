3a7bce3f192d12052a8d53c584b91b25ebb673b6
youyeon11 <yurry45@naver.com>
2025-12-03 14:44:49 +0900

    feat: 가격에 대한 정보 Entity 추가

---
+++ backend/src/main/java/com/yachaerang/backend/api/product/entity/DailyPrice.java
@@ -0,0 +1,22 @@
+package com.yachaerang.backend.api.product.entity;
+
+import com.yachaerang.backend.api.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DailyPrice extends BaseEntity {
+
+    private long dailyPriceId;
+    private String productCode;
+
+    private LocalDate priceDate;
+    private long price;
+}
---
+++ backend/src/main/java/com/yachaerang/backend/api/product/entity/MonthlyPrice.java
@@ -0,0 +1,26 @@
+package com.yachaerang.backend.api.product.entity;
+
+import com.yachaerang.backend.api.common.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MonthlyPrice extends BaseEntity {
+
+    private long monthlyPriceId;
+    private String productCode;
+
+    private Integer priceYear;
+
+    private Double avgPrice;
+    private Long minPrice;
+    private Long maxPrice;
+
+    private Long startPrice;
+    private Long endPrice;
+}
