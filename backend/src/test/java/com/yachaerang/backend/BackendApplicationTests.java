80aa673fd9be4549140ff59682fd5f6ecdcd0359
youyeon11 <yurry45@naver.com>
2025-12-04 17:46:28 +0900

    fix: test 프로필 지정

--- backend/src/test/java/com/yachaerang/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/yachaerang/backend/BackendApplicationTests.java
@@ -2,12 +2,13 @@ package com.yachaerang.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
