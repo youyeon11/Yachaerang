b31044bfb7a5f3f9f692841167282e45ac72efca
youyeon11 <yurry45@naver.com>
2025-12-06 03:58:25 +0900

    fix: 주간 가격 조회에 대하여 기준 조정(startDate와 endDate가 표준을 맞추지 않으면 요청이 유효하지 않도록 설정)

--- backend/src/main/java/com/yachaerang/backend/api/product/service/WeeklyPriceService.java
+++ backend/src/main/java/com/yachaerang/backend/api/product/service/WeeklyPriceService.java
@@ -2,9 +2,11 @@ package com.yachaerang.backend.api.product.service;
 
 import com.yachaerang.backend.api.product.dto.response.WeeklyPriceResponseDto;
 import com.yachaerang.backend.api.product.repository.WeeklyPriceMapper;
+import com.yachaerang.backend.global.exception.CustomException;
 import com.yachaerang.backend.global.exception.GeneralException;
 import com.yachaerang.backend.global.response.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +17,7 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class WeeklyPriceService {
 
     private final WeeklyPriceMapper weeklyPriceMapper;
@@ -22,17 +25,26 @@ public class WeeklyPriceService {
     public List<WeeklyPriceResponseDto.PriceRecordDto> getPriceDuration(
             String productCode, LocalDate startDate, LocalDate endDate
     ) {
+        // startDate 검증
+        boolean isFirstOfWeek = (startDate.getDayOfWeek() == DayOfWeek.MONDAY);
+        if (!isFirstOfWeek) {
+            throw CustomException.of(ErrorCode.INVALID_ISO_WEEK_DATE);
+        }
+        // endDate 검증
+        boolean isEndOfWeek =(endDate.getDayOfWeek() == DayOfWeek.SUNDAY);
+        if (!isEndOfWeek) {
+            throw CustomException.of(ErrorCode.INVALID_ISO_WEEK_DATE);
+        }
+
         // startDate > endDate
         if (endDate.isBefore(startDate)) {
             throw GeneralException.of(ErrorCode.WRONG_REQUEST_DATE);
         }
         LocalDate today = LocalDate.now();
-        if (endDate.isAfter(today)) {
+        if (startDate.isAfter(today) || endDate.isAfter(today)) {
             throw GeneralException.of(ErrorCode.WRONG_REQUEST_DATE);
         }
 
-        LocalDate weekStart = startDate.plusWeeks(1).with(DayOfWeek.MONDAY);
-        LocalDate weekEnd = endDate.minusWeeks(1).with(DayOfWeek.SUNDAY);
-        return weeklyPriceMapper.getPriceDuration(productCode, weekStart, weekEnd);
+        return weeklyPriceMapper.getPriceDuration(productCode, startDate, endDate);
     }
 }
