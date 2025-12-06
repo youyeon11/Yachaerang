c3b70ed9ba0926aaf81b63c7024d8bec4da92ac2
youyeon11 <yurry45@naver.com>
2025-12-06 19:43:35 +0900

    feat: Article 전체 조회 및 상세 조회 구현

--- backend/src/main/java/com/yachaerang/backend/api/article/repository/ArticleMapper.java
+++ backend/src/main/java/com/yachaerang/backend/api/article/repository/ArticleMapper.java
@@ -1,23 +1,28 @@
 package com.yachaerang.backend.api.article.repository;
 
+import com.yachaerang.backend.api.article.dto.request.ArticleRequestDto;
 import com.yachaerang.backend.api.article.entity.Article;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.List;
 
 @Mapper
 public interface ArticleMapper {
 
-    Article findById(@Param("id") int id);
+    /*
+    특정 게시글 조회
+     */
+    Article findById(@Param("id") Long id);
 
-    List<Article> findAll();
+    /*
+    Article 페이지로 조회
+     */
+    List<Article> findAllWithPagination(ArticleRequestDto.PageDto pageRequest);
 
-    List<Article> findAllWithTags();
-
-    int insert(Article article);
-
-    int update(Article article);
-
-    int delete(@Param("id") int id);
+    /*
+    모든 Article 개수 조회
+     */
+    Long countAll();
 }
