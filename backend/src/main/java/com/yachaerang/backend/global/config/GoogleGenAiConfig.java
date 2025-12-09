7b77150faf50d666c1892d1512573f903597ab9c
youyeon11 <yurry45@naver.com>
2025-12-10 03:33:33 +0900

    feat: GoogleGenAiConfiguration 생성 및 커스텀 설정 추가

---
+++ backend/src/main/java/com/yachaerang/backend/global/config/GoogleGenAiConfig.java
@@ -0,0 +1,46 @@
+package com.yachaerang.backend.global.config;
+
+import com.google.genai.Client;
+import com.google.genai.types.HttpOptions;
+import com.yachaerang.backend.global.util.LogUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.google.genai.GoogleGenAiChatModel;
+import org.springframework.ai.google.genai.GoogleGenAiChatOptions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
+
+@Configuration
+public class GoogleGenAiConfig {
+
+    private static final String BASE_URL = "https://gms.ssafy.io/gmsapi/generativelanguage.googleapis.com";
+
+    @Bean
+    public GoogleGenAiChatModel googleGenAiChatModel(
+            @Value("${spring.ai.custom.google.genai.api-key}") String apiKey,
+            @Value("${spring.ai.custom.google.genai.model}") String model
+    ) {
+        HttpOptions httpOptions = HttpOptions.builder()
+                .baseUrl(BASE_URL)
+                .build();
+
+        Client client = Client.builder()
+                .apiKey(apiKey)
+                .httpOptions(httpOptions)
+                .build();
+
+        GoogleGenAiChatOptions options = GoogleGenAiChatOptions.builder()
+                .model(model)
+                .temperature(0.7)
+                .build();
+        return GoogleGenAiChatModel
+                .builder()
+                .genAiClient(client)
+                .defaultOptions(options)
+                .build();
+    }
+}
