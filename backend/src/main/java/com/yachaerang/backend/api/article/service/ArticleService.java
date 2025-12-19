fbf3efe7182e55810fa18dcf0837b6dd1c293db2
youyeon11 <yurry45@naver.com>
2025-12-19 15:49:40 +0900

    feat: 키워드 기반 농촌기사 검색 기능 구현

--- backend/src/main/java/com/yachaerang/backend/api/article/service/ArticleService.java
+++ backend/src/main/java/com/yachaerang/backend/api/article/service/ArticleService.java
@@ -8,8 +8,8 @@ import com.yachaerang.backend.api.article.repository.ArticleMapper;
 import com.yachaerang.backend.api.article.repository.TagMapper;
 import com.yachaerang.backend.global.exception.GeneralException;
 import com.yachaerang.backend.global.response.ErrorCode;
+import com.yachaerang.backend.global.util.LogUtil;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,7 +20,6 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 @Transactional(readOnly = true)
 public class ArticleService {
 
@@ -36,10 +35,11 @@ public class ArticleService {
         // Article Pagination 조회
         List<Article> articleList = articleMapper.findAllWithPagination(pageRequest.getLimit(), pageRequest.getOffset());
 
-        // 빈 리스트일 때
+        // 전체 개수 조회
+        long totalElements = articleMapper.countAll();
         if (articleList.isEmpty()) {
             return ArticleResponseDto.PageDto.of(
-                    Collections.emptyList(), pageRequest, 0L
+                    Collections.emptyList(), pageRequest, totalElements
             );
         }
 
@@ -51,13 +51,13 @@ public class ArticleService {
         // 모든 Article의 TagName 한 번에 조회와 mapping
         List<Tag> tagList = tagMapper.findByArticleIdList(articleIdList);
         Map<Long, List<String>> tagNameMap = tagList.stream()
-                .peek(tag -> log.debug("스트림 처리 중 Tag: {}", tag))
+                .peek(tag -> LogUtil.debug("스트림 처리 중 Tag: {}", tag))
                 .collect(Collectors.groupingBy(
                         Tag::getArticleId,
                         Collectors.mapping(Tag::getName, Collectors.toList())
                 ));
 
-        log.debug("Articles found: {}", articleIdList);
+        LogUtil.debug("Articles found: {}", articleIdList);
 
         List<ArticleResponseDto.ListDto> articleResponseDtoList
                  = articleList.stream()
@@ -71,8 +71,7 @@ public class ArticleService {
                 )
                 .toList();
 
-        // 전체 개수 조회 및 PageResponse 생성
-        long totalElements = articleMapper.countAll();
+        // 페이지네이션 Response 생성
         return ArticleResponseDto.PageDto.of(articleResponseDtoList, pageRequest, totalElements);
     }
 
@@ -96,5 +95,53 @@ public class ArticleService {
                 .tagList(tagList)
                 .createdAt(article.getCreatedAt().toLocalDate())
                 .build();
+    }
+
+    /*
+    기사 검색하기
+     */
+    public ArticleResponseDto.PageDto<ArticleResponseDto.ListDto> searchArticles(
+            ArticleRequestDto.PageDto pageRequest, String keyword) {
+
+        LogUtil.info("{} 키워드 조회 시작...", keyword);
+        List<Article> articleList = articleMapper.findByKeyword(pageRequest.getLimit(), pageRequest.getOffset(), keyword);
+
+        // 페이지네이션을 위한 전체 조회
+        long totalElements = articleMapper.countByKeyword(keyword);
+
+        // 빈 리스트일 때
+        if (articleList.isEmpty()) {
+            return ArticleResponseDto.PageDto.of(
+                    Collections.emptyList(), pageRequest, totalElements
+            );
+        }
+        List<Long> articleIdList = articleList.stream()
+                .map(Article::getId)
+                .collect(Collectors.toList());
+
+        // 모든 Article의 TagName 한 번에 조회와 mapping
+        List<Tag> tagList = tagMapper.findByArticleIdList(articleIdList);
+        Map<Long, List<String>> tagNameMap = tagList.stream()
+                .peek(tag -> LogUtil.debug("스트림 처리 중 Tag: {}", tag))
+                .collect(Collectors.groupingBy(
+                        Tag::getArticleId,
+                        Collectors.mapping(Tag::getName, Collectors.toList())
+                ));
+
+        LogUtil.debug("Articles found: {}", articleIdList);
+
+        List<ArticleResponseDto.ListDto> articleResponseDtoList
+                = articleList.stream()
+                .map(article -> ArticleResponseDto.ListDto.builder()
+                        .articleId(article.getId())
+                        .title(article.getTitle())
+                        .imageUrl(article.getImageUrl())
+                        .createdAt(article.getCreatedAt().toLocalDate())
+                        .tagList(tagNameMap.getOrDefault(article.getId(), Collections.emptyList()))
+                        .build()
+                )
+                .toList();
+
+        return ArticleResponseDto.PageDto.of(articleResponseDtoList, pageRequest, totalElements);
     }
 }
