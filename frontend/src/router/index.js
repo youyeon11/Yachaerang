b6480478942b2bfcef8407c2c2185fc526be26e4
youyeon11 <yurry45@naver.com>
2025-12-20 16:14:43 +0900

    refactor: 소개 페이지 사이드바 제외 적용

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,7 +4,7 @@ import { useToastStore } from '@/stores/toast';
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'main', component: () => import('@/views/main/MainPage.vue') },
+    { path: '/', name: 'main', component: () => import('@/views/main/MainPage.vue'), meta: {hideSidebar: true} },
     { path: '/login', name: 'login', component: () => import('@/views/auth/LoginView.vue') },
     { path: '/forgot-password', name: 'forgot-password', component: () => import('@/views/auth/ForgotPasswordView.vue') },
     { path: '/signup', name: 'signup', component: () => import('@/views/auth/SignupView.vue') },
