6d4f6273f55c839efb1e5dea36fdf784b2230f6a
youyeon11 <yurry45@naver.com>
2025-12-23 04:07:19 +0900

    feat: 요청을 위한 WebClientConfig 생성

--- backend/src/main/java/com/yachaerang/backend/global/config/WebClientConfig.java
+++ backend/src/main/java/com/yachaerang/backend/global/config/WebClientConfig.java
@@ -1,29 +1,14 @@
 package com.yachaerang.backend.global.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-
-    private static final String OPEN_AI_BASE_URL = "https://gms.ssafy.io/gmsapi/api.openai.com/v1/chat/completions";
-
-    @Value("${spring.ai.openai.api-key}")
-    private String apiKey;
-
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
 
-    @Bean(name="openAiWebClient")
-    public WebClient openAiWebClient(WebClient.Builder builder) {
-        return builder
-                .baseUrl(OPEN_AI_BASE_URL)
-                .defaultHeader("Authorization", "Bearer " + apiKey)
-                .defaultHeader("Content-Type", "application/json")
-                .build();
-    }
 }
