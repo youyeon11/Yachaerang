b77970ef7318c0d190b537e470076b45890c6775
youyeon11 <yurry45@naver.com>
2025-12-23 18:27:29 +0900

    feat: Bookmark Mapper 인터페이스 구현

--- backend/src/main/java/com/yachaerang/backend/api/bookmark/repository/BookmarkMapper.java
+++ backend/src/main/java/com/yachaerang/backend/api/bookmark/repository/BookmarkMapper.java
@@ -1,8 +1,11 @@
 package com.yachaerang.backend.api.bookmark.repository;
 
+import com.yachaerang.backend.api.bookmark.vo.ArticleBookmark;
 import com.yachaerang.backend.api.bookmark.entity.Bookmark;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface BookmarkMapper {
@@ -16,4 +19,21 @@ public interface BookmarkMapper {
      * 북마크 해제하기
      */
     int delete(@Param("memberId") Long memberId, @Param("articleId") Long articleId);
+
+    /**
+     * memberId와 articleId를 기반으로 조회하기
+     * @param memberId
+     * @param articleId
+     * @return
+     */
+    Bookmark findByMemberIdAndArticleId(
+            @Param("memberId") Long memberId,
+            @Param("articleId") Long articleId);
+
+    /**
+     * memberId를 기반으로 전체 Bookmark 조회
+     * @param memberId
+     * @return
+     */
+    List<ArticleBookmark> findAllByMemberId(@Param("memberId") Long memberId);
 }
