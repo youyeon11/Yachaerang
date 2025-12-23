455f221017d3ea2929a136590fb09777a9ed1417
youyeon11 <yurry45@naver.com>
2025-12-23 18:28:18 +0900

    feat: 북마크 기능 구현

--- backend/src/main/java/com/yachaerang/backend/api/bookmark/dto/response/BookmarkResponseDto.java
+++ backend/src/main/java/com/yachaerang/backend/api/bookmark/dto/response/BookmarkResponseDto.java
@@ -2,7 +2,19 @@ package com.yachaerang.backend.api.bookmark.dto.response;
 
 import lombok.*;
 
+import java.util.List;
+
 public class BookmarkResponseDto {
 
-
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class GetAllDto {
+        private Long bookmarkId;
+        private Long articleId;
+        private String title;
+        private List<String> tagList;
+    }
 }
--- backend/src/main/java/com/yachaerang/backend/api/bookmark/service/BookmarkService.java
+++ backend/src/main/java/com/yachaerang/backend/api/bookmark/service/BookmarkService.java
@@ -1,14 +1,20 @@
 package com.yachaerang.backend.api.bookmark.service;
 
+import com.yachaerang.backend.api.article.entity.Article;
+import com.yachaerang.backend.api.article.entity.Tag;
 import com.yachaerang.backend.api.bookmark.dto.response.BookmarkResponseDto;
+import com.yachaerang.backend.api.bookmark.vo.ArticleBookmark;
 import com.yachaerang.backend.api.bookmark.entity.Bookmark;
 import com.yachaerang.backend.api.bookmark.repository.BookmarkMapper;
 import com.yachaerang.backend.global.auth.jwt.AuthenticatedMemberProvider;
+import com.yachaerang.backend.global.exception.GeneralException;
+import com.yachaerang.backend.global.response.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.awt.print.Book;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,10 +27,13 @@ public class BookmarkService {
      * 북마크 저장하기
      */
     @Transactional
-    public void register(Long articleId) {
+    public void registerBookmark(Long articleId) {
         Long memberId = authenticatedMemberProvider.getCurrentMemberId();
 
-        // 이미 등록된 것인지 조회
+        // 이미 등록된 것이라면 예외
+        if (bookmarkMapper.findByMemberIdAndArticleId(memberId, articleId) != null) {
+            throw GeneralException.of(ErrorCode.ALREADY_BOOKMARKED);
+        }
 
         Bookmark bookmark = Bookmark.builder()
                 .articleId(articleId)
@@ -32,7 +41,7 @@ public class BookmarkService {
         int result = bookmarkMapper.save(bookmark);
 
         if (result != 1) {
-
+            throw GeneralException.of(ErrorCode.BOOKMARK_DB_FAILED);
         }
         return;
     }
@@ -40,21 +49,50 @@ public class BookmarkService {
     /**
      * 북마크 해제하기
      */
+    @Transactional
     public void eraseBookmark(Long articleId) {
         Long memberId = authenticatedMemberProvider.getCurrentMemberId();
 
-        // 등록된 북마크인지 조회
+        // 등록된 적이 없다면 예외
+        if (bookmarkMapper.findByMemberIdAndArticleId(memberId, articleId) == null) {
+        throw GeneralException.of(ErrorCode.BOOKMARK_NOT_FOUND);
+        }
 
-
-        // 해제
+        // 북마크에서 해제
         int result = bookmarkMapper.delete(memberId, articleId);
         if (result != 1) {
-
+            throw GeneralException.of(ErrorCode.BOOKMARK_DB_FAILED);
         }
+        return;
     }
 
     /**
      * 북마크 전체 조회하기
      */
+    @Transactional(readOnly = true)
+    public List<BookmarkResponseDto.GetAllDto> getAll() {
 
+        Long memberId = authenticatedMemberProvider.getCurrentMemberId();
+
+        List<ArticleBookmark> bookmarkList = bookmarkMapper.findAllByMemberId(memberId);
+
+        return bookmarkList.stream()
+                .map(articleBookmark -> {
+                    Article article = articleBookmark.getArticle();
+
+                    return BookmarkResponseDto.GetAllDto.builder()
+                            .bookmarkId(articleBookmark.getBookmarkId())
+                            .articleId(articleBookmark.getArticleId())
+                            .title(article.getTitle())
+                            .tagList(
+                                    article.getTagList() != null
+                                    ? article.getTagList().stream()
+                                            .map(Tag::getName)
+                                            .toList()
+                                            : Collections.emptyList()
+                            )
+                            .build();
+                })
+                .toList();
+    }
 }
